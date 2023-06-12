ec6c91cf8a2944aebd78e65d823d0ec00ce6aae2
bnalepa <bnalepa@o2.pl>
2023-06-12 17:31:01 +0200

    Fill JwtService.cs

--- CollegeProjectManagment.Core/Services/JwtService.cs
+++ CollegeProjectManagment.Core/Services/JwtService.cs
@@ -11,35 +11,48 @@ namespace CollegeProjectManagment.Core.Services
 {
     public class JwtService : IJwtService
     {
-        //private readonly IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
 
-        //public JwtService(IConfiguration configuration)
-        //{
-        //    _configuration = configuration;
-        //}
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-        //public AuthenticationResponse CreateJwtToken(ApplicationUser user)
-        //{
-        //    DateTime expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATIN_MINUTES"]));
-        //    Claim[] claims = new Claim[] {
-        //        new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
-
-        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID  
-
-        //        new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString()), // Issued at ( date and time of token generation)
-
-        //        new Claim(ClaimTypes.NameIdentifier, user.Email), // unique name identifier of the user (Email)
-        //        new Claim(ClaimTypes.Name, user.PersonName), // unique name identifier of the user (Email)
-
-        //    };
-
-        //    SymmetricSecurityKey securityKey = new SymmetricSecurityKey
-        //        SymmetricSecurityKey(
-        //           Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
         public AuthenticationResponse CreateJwtToken(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            DateTime expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATIN_MINUTES"]));
+            Claim[] claims = new Claim[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID  
+
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToString()), // Issued at ( date and time of token generation)
+
+                new Claim(ClaimTypes.NameIdentifier, user.Email), // unique name identifier of the user (Email)
+                new Claim(ClaimTypes.Name, user.PersonName), // unique name identifier of the user (Email)
+
+            };
+
+            SymmetricSecurityKey securityKey = new
+                SymmetricSecurityKey(
+                   Encoding.UTF8.GetBytes(_configuration["Jwt:KEY"]));
+            SigningCredentials signingCredentials = new
+                SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenGenerator = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: expiration,
+                signingCredentials: signingCredentials
+                );
+            JwtSecurityTokenHandler tokenHandler = new
+            JwtSecurityTokenHandler();
+            string token = tokenHandler.WriteToken(tokenGenerator);
+            return new AuthenticationResponse() { Token = token, PersonName = user.PersonName,  
+                Expiration = expiration };
         }
     }
+
 }
 
